0ba03f51038051bdbd84c91f9729c9201d66099b
tscxk <952505116@qq.com>
2020-12-29 19:21:48 +0800

    正式环境配置部分问题修复
    更正本地和线上的配置文件  蘑菇博客正式运营

--- vue_mogu_web/config/prod.env.js
+++ vue_mogu_web/config/prod.env.js
@@ -7,7 +7,7 @@ module.exports = {
   //VUE_MOGU_WEB: '"http://39.107.247.2:8602"',
   // 如果有域名使用这个
   VUE_MOGU_WEB: '"http://www.lvmz521.com"',
-  PICTURE_API: '"http://picture.lvmz521.com"',
+  PICTURE_API: '"http://39.107.247.2:8602"',
   WEB_API: '"http://39.107.247.2:8603"',
   ELASTICSEARCH: '"http://39.107.247.2:8605"',
 }
